9d2d1875aaea5fc85f2dc7a0d109d261c1bc72c8
Mohamed Emara <medoemara104@gmail.com>
2024-05-16 00:30:49 +0300

    Implement websocket server

---
+++ socket-server/socketServer.js
@@ -0,0 +1,29 @@
+import { WebSocketServer } from 'ws';
+
+// Initialize WebSocket server
+const wss = new WebSocketServer({ port: 8081 });
+
+// Event handlers for WebSocket server
+// NOTE: Port is a unique identifier for clients.
+wss.on('connection', (ws, req) => {
+    console.log(`A new client connected! #${req.socket.remotePort}`);
+    
+    // Error logger
+    ws.on('error', console.error);
+
+    // Message event handler
+    ws.on('message', function message(data) {
+        // Log the message from the client..
+        console.log('A message from client received: %s', data);
+
+        // Respond from the server..
+        ws.send('We got your message; from server!');
+    });
+
+    // Close event handler
+    ws.on('close', () => {
+        console.log(`A client disconnected! #${req.socket.remotePort}`);
+    })
+});
+
+console.log('WebSocket server is running on port 8081');
